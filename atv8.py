7b8b2756779004106be020ba95b50c8fbe10ea60
yuricamisa8 <francisco.cardoso94@aluno.ce.gov.br>
2024-09-09 09:32:55 -0300

    first commit

---
+++ atv8.py
@@ -0,0 +1,4 @@
+# Imagine que você está em uma viagem para os Estados Unidos e precisa converter o valor em reais para dólares. Crie um programa que receba o valor em reais e a taxa de câmbio atual, e exiba o valor equivalente em dólares.
+r = float(input("inssira o valor em real"))
+d = (5.61)
+print(r/d)
